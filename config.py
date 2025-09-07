7aa3031d0f7d293ff7b126182673167f1c82844a
linkoraa <tmbekzod05@gmail.com>
2025-09-07 23:58:25 +0500

    fix: add token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7018948736:AAH_aD4An4aIelGpwO2B1Gfg-tn8Urlo-ss")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8260689904:AAH2XxW1rnCIb_Dfz_rOGgH9nnCOW9004M8")
 ADMIN_IDS = [5403516004, 5237348661]  # Adminlar Telegram ID'lari
 DB_PATH = "bot.db"
